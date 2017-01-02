84815d6d7eac5d379461d633dd86c2a3e73df1b5
MMY <barry.ding@ichef.com.tw>
2017-01-02 17:08:03 +0800

    Initial Commit

---
+++ MMYetronome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMYetronome
+//
+//  Created by PaoYo Ding on 2017/1/2.
+//  Copyright © 2017年 PeoYao Ding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
